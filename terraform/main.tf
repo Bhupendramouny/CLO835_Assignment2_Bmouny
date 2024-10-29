d217873b5153ed76c7e6f5e020c518aafd8ed55c
EC2 Default User <ec2-user@ip-172-31-41-54.ec2.internal>
2024-10-29 19:24:00 +0000

    final try 1 for ci/cd

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,7 +101,7 @@ output "webapp_ecr_repository_url" {
 }
 
 output "mysql_ecr_repository_url" {
-  value = aws_ecr_repository.mysql.repository_url
+  value = aws_ecr_repository.mysql.repository_url //getting ecr
 }
 
 # output "mysql_ecr_repository_url" {
